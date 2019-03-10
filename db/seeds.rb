5a26a178a0ffaac6a81c5c90bf1dc3e75ce8bdf9
wardch <charlieward18@gmail.com>
2019-03-10 16:17:35 +0000

    added rails api for street smart

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: "Anto", last_name: "SpiceBag", email: 'antodespice@gmail.com', high_score: 12)
+User.create(first_name: "Jimmy", last_name: "DribblyJocks", email: 'jimobatteredjocks@burger.com', high_score: 143)
